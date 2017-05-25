082b6ac9bad327dacc865029e1b8dd9781c342a0
CeilaScarlet <reginasmurillo@gmail.com>
2017-05-25 14:07:16 -0700

    Add files via upload

---
+++ src/for_loops/HiLowGame.java
@@ -0,0 +1,13 @@
+package for_loops;
+
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class HiLowGame {
+public static void main(String[] args) {
+	String Input= JOptionPane.showInputDialog("Guess a number between 0 and 100.");
+	int IPT=Integer.parseInt(Input);
+}
+}
